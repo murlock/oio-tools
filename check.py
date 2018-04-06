8fbdabaf23260e07acc426f7bce2f64236218017
Michael Bonfils <murlock42@gmail.com>
2018-04-06 10:50:36 +0200

    Add prefix option and use full listing

--- check.py
+++ check.py
@@ -18,12 +18,26 @@ NAMESPACE = "OPENIO"
 HOST = '127.0.0.1:6035'
 
 
+def full_list(backend, **kwargs):
+        listing = backend.list_containers(ACCOUNT, **kwargs)
+        for element in listing:
+            yield element
+
+        while listing:
+            kwargs['marker'] = listing[-1][0]
+            listing = backend.list_containers(ACCOUNT, **kwargs)
+            if listing:
+                for element in listing:
+                    yield element
+
+
 def options(args):
     parser = argparse.ArgumentParser()
     parser.add_argument("--account", default=os.getenv("OIO_ACCOUNT", "demo"))
     parser.add_argument("--namespace", default=os.getenv("OIO_NS", "OPENIO"))
     parser.add_argument("--verbose", default=False, action="store_true")
     parser.add_argument("--dry-run", default=False, action="store_true")
+    parser.add_argument("--prefix", help="prefix of containers of check")
     parser.add_argument("account", help="IP:PORT of Account service")
 
     return parser.parse_args()
@@ -33,9 +47,12 @@ def run(args):
 
     dirclient = DirectoryClient({"namespace": NAMESPACE})
     backend = AccountBackend(dirclient.conf)
-    lst = backend.list_containers(ACCOUNT)
 
-    for entry, _, _, _ in lst:
+    for entry, _, _, partial in full_list(backend, prefix=args.prefix):
+        if partial:
+            if args.verbose:
+                print(":%s: partial, skip" % entry)
+            continue
         try:
             dirclient.show(account=ACCOUNT, reference=entry)
             if args.verbose:
